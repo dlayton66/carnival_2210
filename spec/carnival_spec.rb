c83d377ff8be9f00d85c1fa8eac8aafe256169a3
Drew Layton <dlayton66@gmail.com>
2022-11-14 10:15:11 -0700

    feat/test: Carnival#most_popular_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -44,4 +44,29 @@ RSpec.describe Carnival do
       expect(carnival.rides).to eq([ride1,ride2,ride3])
     end
   end
+
+  describe '#most_popular_ride' do
+    it 'returns the most popular ride' do
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor2.add_preference(:thrilling)
+      visitor3.add_preference(:thrilling)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+      ride2.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride2.board_rider(visitor3)
+      ride3.board_rider(visitor1)
+      ride3.board_rider(visitor2)
+      ride3.board_rider(visitor3)
+
+      expect(carnival.most_popular_ride).to eq(ride1)
+    end
+  end
 end
